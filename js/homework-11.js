d18e5cd616e8c56f68e29519143e8815e6c355c1
Marat <maratti1987@gmail.com>
2026-08-24 13:48:19 +0500

    created homework-11.js (footer and registration logic)

---
+++ js/homework-11.js
@@ -0,0 +1,78 @@
+// Уровень 1: задание 4
+
+const form = document.getElementById("subscribe-form");
+const emailInput = document.getElementById("emailInput");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = emailInput.value.trim();
+  if (!email) {
+    console.log("Ошибка: email пустой");
+    return;
+  }
+  console.log({ email });
+});
+
+// Уровень 2: задание 5(1)
+
+const openModalBtn = document.getElementById("openModalBtn");
+const modal = document.getElementById("modal");
+const overlay = document.getElementById("overlay");
+const closeModalBtn = document.getElementById("closeModalBtn");
+
+openModalBtn.addEventListener("click", () => {
+  modal.classList.add("modal-showed");
+  overlay.classList.add("overlay-showed");
+});
+
+closeModalBtn.addEventListener("click", () => {
+  modal.classList.remove("modal-showed");
+  overlay.classList.remove("overlay-showed");
+});
+
+overlay.addEventListener("click", () => {
+  modal.classList.remove("modal-showed");
+  overlay.classList.remove("overlay-showed");
+});
+
+// Уровень 2: задание 5(2)
+
+const registrationForm = document.getElementById("registrationForm");
+
+let user=null;
+
+registrationForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+   if (!registrationForm.checkValidity()) {
+    console.log("Ошибка: форма невалидна");
+    return;
+  }
+
+  const formData = new FormData(registrationForm);
+
+  const password = formData.get("password");
+  const repeatPassword = formData.get("repeatPassword");
+
+  if (password !== repeatPassword) {
+    console.log("Ошибка: пароли не совпадают");
+    return;
+  }
+
+  user = {
+    firstName: formData.get("firstName"),
+    lastName: formData.get("lastName"),
+    birthDate: formData.get("birthDate"),
+    login: formData.get("login"),
+    password: password,
+    createdOn: new Date()
+  };
+
+  console.log("Регистрация успешна:", user);
+
+  modal.classList.remove("modal-showed");
+  overlay.classList.remove("overlay-showed");
+
+  registrationForm.reset();
+});
